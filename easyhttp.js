862a4ce5d51f1f60feb220a9bdf4de7b17e654c6
phamin2001 <amin.pahlavani@gmail.com>
2020-06-13 09:42:49 -0600

    Delete Route

--- easyhttp.js
+++ easyhttp.js
@@ -33,17 +33,30 @@ easyHTTP.prototype.post = function (url, data, callback) {
 };
 
 // Make any HTTP PUT Request
-easyHTTP.prototype.put = function(url, data, callback) {
+easyHTTP.prototype.put = function (url, data, callback) {
   this.http.open('PUT', url, true);
   this.http.setRequestHeader('Content-Type', 'application/json');
 
   let self = this;
   this.http.onload = function () {
     callback(null, self.http.responseText);
-  }
-
+  };
 
   this.http.send(JSON.stringify(data));
-}
+};
 
 // Make any HTTP DELETE Request
+easyHTTP.prototype.delete = function (url, callback) {
+  this.http.open('DELETE', url, true);
+
+  let self = this;
+  this.http.onload = function () {
+    if (self.http.status === 200) {
+      callback(null, 'Post Deleted');
+    } else {
+      callback('Error:' + self.http.status);
+    }
+  };
+
+  this.http.send();
+};
